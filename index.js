5c4399866205ac435d18889271082e014cdd084b
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2024-02-09 00:54:27 -0300

    Correção código final

--- index.js
+++ index.js
@@ -1,95 +1,95 @@
 const perguntas = [
   {
-    pergunta: "Qual é o único tipo de Pokémon que não tem uma habilidade que o afeta negativamente?",
+    pergunta: "Qual é o único tipo de Pokémon que é imune ao tipo Veneno?",
     respostas: [
-      "Fantasma",
+      "Inseto",
+      "Fada",
+      "Aço"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é fraco contra quatro outros tipos diferentes?",
+    respostas: [
+      "Inseto",
+      "Gelo",
+      "Pedra"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o único tipo de Pokémon que é resistente ao tipo Fada?",
+    respostas: [
+      "Lutador",
       "Dragão",
+      "Noturno"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é forte contra o tipo Gelo?",
+    respostas: [
+      "Fogo",
+      "Aço",
+      "Elétrico"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é imune ao tipo Fantasma?",
+    respostas: [
+      "Psíquico",
+      "Normal",
+      "Fantasma"
+    ],
+    correta: 1
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é fraco contra o tipo Fada?",
+    respostas: [
+      "Venenoso",
+      "Dragão",
+      "Lutador"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é forte contra o tipo Água?",
+    respostas: [
+      "Elétrico",
+      "Planta",
+      "Fogo"
+    ],
+    correta: 2
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é resistente ao tipo Aço?",
+    respostas: [
+      "Fada",
+      "Fogo",
+      "Lutador"
+    ],
+    correta: 0
+  },
+  {
+    pergunta: "Qual é o tipo de Pokémon que é imune ao tipo Elétrico?",
+    respostas: [
+      "Voador",
+      "Terrestre",
       "Inseto"
     ],
-    correta: 0
-  },
-  {
-    pergunta: "Qual é o número total de formas diferentes que o Pokémon Unown pode assumir?",
-    respostas: [
-      "26",
-      "28",
-      "30"
-    ],
     correta: 1
   },
   {
-    pergunta: "Qual é o menor Pokémon em termos de altura?",
+    pergunta: "Qual é o tipo de Pokémon que é forte contra o tipo Veneno?",
     respostas: [
-      "Joltik",
-      "Flabébé",
-      "Diglett"
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Qual é o único Pokémon que não é do tipo Fantasma, mas tem a habilidade de aprender o movimento 'Fantoche'?",
-    respostas: [
-      "Shuppet",
-      "Mimikyu",
-      "Spiritomb"
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Quantos diferentes tipos de Pokébolas existem até a geração 8?",
-    respostas: [
-      "20",
-      "25",
-      "30"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual é o único Pokémon que pode aprender todos os movimentos tipo Voador?",
-    respostas: [
-      "Scyther",
-      "Hawlucha",
-      "Tornadus"
-    ],
-    correta: 1
-  },
-  {
-    pergunta: "Qual é o Pokémon com o maior número de resistências de tipo?",
-    respostas: [
-      "Shedinja",
-      "Claydol",
-      "Sableye"
-    ],
-    correta: 0
-  },
-  {
-    pergunta: "Quantos quilômetros por hora (km/h) o Pokémon Electrode pode alcançar?",
-    respostas: [
-      "80",
-      "100",
-      "120"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Qual é o único Pokémon que pode aprender todos os movimentos tipo Fada?",
-    respostas: [
-      "Clefable",
-      "Togekiss",
-      "Sylveon"
-    ],
-    correta: 2
-  },
-  {
-    pergunta: "Quantos diferentes movimentos tipo Normal existem até a geração 8?",
-    respostas: [
-      "80",
-      "100",
-      "120"
+      "Inseto",
+      "Psíquico",
+      "Lutador"
     ],
     correta: 2
   }
-]
+];
 
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
